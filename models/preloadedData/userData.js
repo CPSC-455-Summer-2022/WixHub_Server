5342e7826aca76084ec73bcfca5928dabcb2b299
Josh Tillson <tillson@students.cs.ubc.ca>
2022-08-04 20:28:35 -0700

    refactored dbsetup to make it cleaner

---
+++ models/preloadedData/userData.js
@@ -0,0 +1,40 @@
+const userData = [
+    {
+        "f_name": "Josh",
+        "l_name": "Tillson",
+        "country": "Canada",
+        "destinations": ["1"],
+        "question_responses": [1, 2, 3, 4, 1, 2, 3, 4],
+        "email": "josh@tillson.com",
+        "password": "1234password"
+    },
+    {
+        "f_name": "Ronin",
+        "l_name": "Cunningham",
+        "country": "Canada",
+        "destinations": ["2"],
+        "question_responses": [1, 1, 1, 1, 1, 1, 1, 1],
+        "email": "ronin@cunningham.com",
+        "password": "1234password"
+    },
+    {
+        "f_name": "Sherman",
+        "l_name": "Lam",
+        "country": "Canada",
+        "destinations": ["1", "2", "3"],
+        "question_responses": [1, 2, 1, 2, 1, 2, 1, 2],
+        "email": "sherman@lam.com",
+        "password": "1234password"
+    },
+    {
+        "f_name": "Kevin",
+        "l_name": "Zhao",
+        "country": "Canada",
+        "destinations": ["1"],
+        "question_responses": [2, 3, 4, 4, 4, 3, 3, 2],
+        "email": "kevin@zhao.com",
+        "password": "1234password"
+    },
+]
+
+module.exports = userData;
